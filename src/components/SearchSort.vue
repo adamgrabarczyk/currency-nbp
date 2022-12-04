151aeda3b1b80c97ce7061f070fd72d228891743
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2022-12-04 21:29:53 +0100

    initial commit

---
+++ src/components/SearchSort.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-hover>
+    <template v-slot:default="{ isHovering, props }">
+      <v-card
+        v-bind="props"
+        :color="isHovering ? 'primary' : undefined"
+        title="Hover over me"
+        text="..."
+      ></v-card>
+    </template>
+  </v-hover>
+</template>
+<script>
+export default {
+  name: "SearchSort",
+};
+</script>
+
+<style scoped></style>
